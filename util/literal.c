e095ed5ec5bb7f5eaacf0b1600a83c21f2b265fc
potados99 <potados99@gmail.com>
2018-04-05 00:36:30 +0900

    Add strings

---
+++ util/literal.c
@@ -0,0 +1,11 @@
+//
+//  literal.c
+//  data-structure
+//
+//  Created by MacBoogy on 2018. 4. 4..
+//  Copyright © 2018년 MacBoogy. All rights reserved.
+//
+
+#include "literal.h"
+
+char *indexOut = "Index out of range.";
---
+++ util/literal.h
@@ -0,0 +1,16 @@
+//
+//  literal.h
+//  data-structure
+//
+//  Created by MacBoogy on 2018. 4. 4..
+//  Copyright © 2018년 MacBoogy. All rights reserved.
+//
+
+#ifndef literal_h
+#define literal_h
+
+#include <stdio.h>
+
+extern char *indexOut;
+
+#endif /* literal_h */
